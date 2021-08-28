adbd02029be01de9d96c4e06359a21fbfee1f04b
Muhammad Zubaidi <muhammadzubaidi333@gmail.com>
2021-08-28 09:36:41 +0700

    Project Latihan Asesmen revisi sabtu 280821

---
+++ app/src/main/java/com/example/pengaduanmasyarakat/RegisActivity.kt
@@ -0,0 +1,14 @@
+package com.example.pengaduanmasyarakat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class RegisActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regis)
+    }
+
+    fun setLogin(view: View) {}
+}
